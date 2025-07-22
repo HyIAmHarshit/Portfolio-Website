ddfe2891678b2d5c86075c58b341bda64ceea60e
Harshit Khandelwal <khandelwalharshit2024@gmail.com>
2025-07-23 02:47:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import HeroSection from "./HeroSection";
-import AboutMe from "./AboutMe"
-import Education from "./Education"
-import Skills from "./Skills"
-import Projects from "./Projects"
-import ContactMe from "./ContactMe"
-import Footer from "./Footer"
+import AboutMe from "./AboutMe";
+import Education from "./Education";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import ContactMe from "./ContactMe";
+import Footer from "./Footer";
 
 function App() {
   return (
     <main className="bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#24243e] text-white scroll-smooth">
-      <HeroSection />
+      <section id="home">
+        <HeroSection />
+      </section>
 
-      <AboutMe />
-      <Education />
-      <Skills />
-      <Projects />
-      <ContactMe />
+      <section id="about">
+        <AboutMe />
+      </section>
+
+      <section id="education">
+        <Education />
+      </section>
+
+      <section id="skills">
+        <Skills />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="contact">
+        <ContactMe />
+      </section>
 
       <Footer />
     </main>
-  )
+  );
 }
 
 export default App;
